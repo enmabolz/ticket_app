49ff34a17f8bc52d947740ae5c862f8f48f6419c
Enmanuel Bolzonello <enm.bolz@gmail.com>
2023-08-01 22:14:20 +0200

    Added home_admin.html document

--- src/tickets/models/models.py
+++ src/tickets/models/models.py
@@ -1,4 +1,5 @@
 from tickets import db
+from datetime import datetime
 
 class Users(db.Model):
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
@@ -15,8 +16,8 @@ class Tickets(db.Model):
     id = db.Column(db.Integer(), nullable=False, primary_key=True, autoincrement=True)
     number_of_secuence = db.Column(db.Integer(), nullable=False)
     description = db.Column(db.String(length=500), nullable=False)
-    initialdate = db.Column(db.DateTime(), nullable=False)
-    enddate = db.Column(db.DateTime())
+    initialdate = db.Column(db.DateTime, nullable=False, default=datetime.today())
+    enddate = db.Column(db.DateTime)
     status = db.Column(db.String(), nullable=False)
     duration_in_days = db.Column(db.Integer())
     user_id = db.Column(db.Integer(), nullable=False)
@@ -26,8 +27,8 @@ class EntriesOfTickects(db.Model):
     id = db.Column(db.Integer(), nullable=False, primary_key=True, autoincrement=True)
     number_of_secuence = db.Column(db.Integer(), nullable=False)
     description = db.Column(db.String(500), nullable=False)
-    initialdate = db.Column(db.DateTime(), nullable=False)
-    enddate = db.Column(db.DateTime(), nullable=False)
+    initialdate = db.Column(db.DateTime, nullable=False, default=datetime.today())
+    enddate = db.Column(db.DateTime, nullable=False)
     status = db.Column(db.String(length=50), nullable=False)
     id_ticket = db.Column(db.Integer(), nullable=False)
 
--- src/tickets/routes.py
+++ src/tickets/routes.py
@@ -2,10 +2,12 @@ from tickets import app, db
 from tickets.models.models import Users, Tickets, EntriesOfTickects
 from tickets.models.forms import LoginForm, RegisterForm
 from flask import render_template, url_for, redirect, flash
+from datetime import datetime
 
 @app.route("/")
 def login():
     db.create_all()
+    print(str(datetime.now))
     form = LoginForm()
     return render_template("login.html", form=form)
 
@@ -13,3 +15,35 @@ def login():
 def register_user():
     form = RegisterForm()
     return render_template('register_user.html', form=form)
+
+
+@app.route("/home/admin")
+def home_admin_user():
+
+    record = Tickets(
+        number_of_secuence = 1,
+        description = "jonjkfnjk kewmfjklfklf ikejkfnjkf",
+        enddate = None,
+        status = "In Progress",
+        duration_in_days = None,
+        user_id = 1,
+    )
+
+    record1 = Tickets(
+        number_of_secuence = 1,
+        description = "jonjkfnjk kewmfjklfklf ikejkfnjkf",
+        enddate = None,
+        status = "In Progress",
+        duration_in_days = None,
+        user_id = 1,
+    )
+
+    db.session.add(record)
+    db.session.add(record1)
+    db.session.commit()
+
+
+
+    tickets_data = Tickets.query.all()
+    print(tickets_data)
+    return render_template('home_admin.html', tickets=tickets_data)
